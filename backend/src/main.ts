95b33ebb68dbbe6e195d103f60582621b890f7ad
Julián Camilo García Rincón <jcgrrincon@gmail.com>
2024-08-25 12:04:01 -0500

    add port as env varaible

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: true,
   });
-  await app.listen(3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
